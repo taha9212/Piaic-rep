20a217f25c569ef8ebd89b851420e04ebc05cb79
Taha <tahah0255@gmail.com>
2019-06-20 19:59:38 +0500

    Stars Shapes

--- nestedloop.py
+++ nestedloop.py
@@ -13,8 +13,20 @@ for i in range (1,6):
     for j in range (i,6):
         print('*', end =" ")
     print("\n")
-#Diagonal
-for i in range (1,6):
-    for j in range (i,6):
-        print("", end = " ")
-    print("*")
+#Diamond
+n = 5
+for i in range (0,n):
+    for j in range (0,n):
+        print(" ", end = " ")
+    for k in range (0,(i*2-1)):
+        print("*", end = " ")
+    print("")
+    n -= 1
+m = 5
+for i in range (0,m):
+    for j in range (0,i):
+        print(" ", end = " ")
+    for k in range (0, m*2-1):
+        print("*", end = " ")
+    print("")
+    m-=1
